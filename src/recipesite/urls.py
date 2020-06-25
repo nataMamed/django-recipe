36db89a37317863d3cc0faf21df7442c34446633
Natã Mamede <natamamede@gmail.com>
2020-06-25 20:40:36 -0300

    conf: setting up views and urls of the recipe app

--- src/recipesite/urls.py
+++ src/recipesite/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
+    #My urls
+    path('', include('recipe.urls')),
+    #Default urls
     path('admin/', admin.site.urls),
+
 ]
