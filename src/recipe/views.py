109d23eb1defe2db94b3f34dd07e1c356eac0ab4
Natã Mamede <natamamede@gmail.com>
2020-06-25 22:13:02 -0300

    feat: displaying the recipes dinamicaly

--- src/recipe/views.py
+++ src/recipe/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
+from .models import Recipe
 
 
 def index_view(request):
 
-    return render(request, template_name='index.html')
+    
+    recipes = Recipe.objects.order_by('-registration_date').filter(published=True)
+    data ={
+        "recipes": recipes
+    }
+
+    return render(request, template_name='index.html', context=data)
 
 def recipe_view(request):
 
