560084be3eba7a1e17923243dc8142fcd5c339b2
Natã Mamede <natamamede@gmail.com>
2020-06-25 21:58:08 -0300

    feat: creating a recipe model end registering into admin

--- src/recipe/models.py
+++ src/recipe/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class Recipe(models.Model):
+
+    recipe_name        = models.CharField(max_length=200)
+    category           = models.CharField(max_length=100)
+    ingredients        = models.TextField()
+    preparation_method = models.TextField()
+    preparation_time   = models.IntegerField()
+    revenue            = models.CharField(max_length=50)
+    published          = models.BooleanField(default=False)
+    registration_date  = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.recipe_name
+
+    
