39d93dbd63cbf694597ab86be4eb73c58b138d50
Natã Mamede <natamamede@gmail.com>
2020-06-28 17:52:01 -0300

    feat: validating all the users input from the form tag

--- src/users/views.py
+++ src/users/views.py
@@ -1,10 +1,34 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 
 def singup_view(request):
 
-    return render(request, template_name='users/singup.html')
+    if request.method == 'POST':
+
+        name      = request.POST['nome']
+        email     = request.POST['email'] 
+        password  = request.POST['password']
+        password2 = request.POST['password2']
+
+        if not name.strip():
+            return redirect(to='singup')
+
+        if not email.strip():
+            return redirect(to='singup')
+
+        if password != password2:
+            return redirect(to='singup')
+        
+        if User.objects.filter(email=email).exists():
+            return redirect(to='login')
+        else:
+            user = User.objects.create_user(username=name, email=email, password=password)
+            user.save()
+
+        return redirect(to='login')
+    else:
+        return render(request, template_name='users/singup.html')
 
 
 def login_view(request):
